782d361f5685a421fd43a311f94172b21033d3e6
as821 <49156741+as821@users.noreply.github.com>
2020-04-16 22:19:36 -0400

    Debugging, now function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 # import statements
 from definitions import *
-from backend import access, create_index, delete_index, create_table, drop_table, selection, projection, join, task_manager, dml_delete, dml_update, dml_insert, initializer, output
+from backend import access, create_index, delete_index, create_table, drop_table, selection, projection, join, dml_delete, dml_update, dml_insert, initializer
 from parser import parser_main
 from optimizer import optimizer
 
@@ -15,18 +15,34 @@ from optimizer import optimizer
 #           #
 #   TODO    #
 #           #
-#   (main.py)               output resulting relation properly
-#   (backend.py)            automatically add an index on the primary key of a table
-
-
 #   *** Features to add ***
-#   GROUPBY/HAVING
-#   NOW (time logging function)
-#   EXPLAIN (outputs information about optimizer decisions)
-#   Support multiple foreign keys for each table\
+#   Support multiple foreign keys for each table
+
+#   add comments, make code readable
+#   add usage specifications (how to use now function, what where conditions are supported and what is not, limitations on DML where clauses, etc)
 
 
-#   select i-i-hundred.* from i-i-hundred  as i natural join i-1-hundred  as one natural join i-i-thousand where (i-i-thousand.id < 30) and (i.id < 5)
+
+
+# note on NOW usage:  can specify "now()" in a query and get current time added to each returned tuple.  Can specify "now" without a datatype as an attribute in a create table
+#       and the time of each insertion will be recorded automatically.  When writing DML insert command, need to specify the now attribute (placed at the end of attribute list)
+#       but do not specify a value for the now attribute as it will be filled in by the system
+
+
+
+
+
+#       *** Sample inputs ***
+#   "select i-i-hundred.* from i-i-hundred  as i natural join i-1-hundred  as one natural join i-i-thousand where (i-i-thousand.id < 30) and (i.id < 5)"
+#   "insert into test1_rel (name, age, year) values (\"joe\", 31, 2200)"
+#   "delete from test1_rel where test1_rel.name = \"Andrew\""
+#   "update test1_rel set year = 0 where test1_rel.name = \"Andrew\""
+#   "create table test3_rel (name string, school_year string, hourly_salary int, primary key(name), foreign key (name) references test1_rel(name))"
+#   "SELECT name FROM test1_rel as a WHERE (((a.age <= 50) and (a.name = \"Andrew\")) or (a.year < 2000)) or ((a.year > 1400) or (a.name = \"Bob\"))"
+
+
+
+
 
 
 # main function
@@ -38,37 +54,12 @@ def main():
     init_end = time.time()
     print("initialization time: ", init_end-init_start)
 
-    # table1 = create_table("test1_rel", [("name", "string"), ("age", "int"), ("year", "int")])
-    # table2 = create_table("test2_rel", [("name", "string"), ("income", "int"), ("home_town", "string")])
-    # task_manager(None)  # just writes a few tuples to each of the tables to make query testing better
-    #
-
-
-
-
     # start infinite input loop
     keep_looping = True
     while keep_looping:
         # take line of input
         try:
             inp_line = input("\n>>>\t")
-
-            #       *** Sample inputs ***
-            #   "insert into test1_rel (name, age, year) values (\"joe\", 31, 2200)"
-            #   "delete from test1_rel where test1_rel.name = \"Andrew\""
-            #   "update test1_rel set year = 0 where test1_rel.name = \"Andrew\""
-            #   "create table test3_rel (name string, school_year string, hourly_salary int, primary key(name), foreign key (name) references test1_rel(name))"
-            #   "SELECT name FROM test1_rel as a WHERE (((a.age <= 50) and (a.name = \"Andrew\")) or (a.year < 2000)) or ((a.year > 1400) or (a.name = \"Bob\"))"
-
-            # TODO testing section
-            # create_index(TABLES["test1_rel"], "ind1", "name")
-            # TODO END testing section
-
-            # TODO testing
-
-
-
-
 
             # check for loop termination
             if inp_line == "exit":
@@ -87,14 +78,15 @@ def main():
                 if type(parsed_obj) is Query:
 
                     # call optimizer on query output from the parser
-                    attr_list, resulting_relation = optimizer(parsed_obj)
+                    attr_list, resulting_relation, explain_string = optimizer(parsed_obj)
 
                     # output resulting relation
                     if optimize:
                         print(resulting_relation)
                     else:
                         print(t.tabulate(resulting_relation, headers=attr_list, tablefmt="pretty"))
-                        # output(resulting_relation, attr_list)
+
+                    print("\nExplain:", explain_string, "\n")
 
                 # call appropriate DDL/DML command
                 elif type(parsed_obj) is DML:
@@ -109,7 +101,18 @@ def main():
                         if parsed_obj.table:
                             create_table(parsed_obj.table_name, parsed_obj.attr)
                             TABLES[parsed_obj.table_name].primary_key = parsed_obj.primary_key
-                            TABLES[parsed_obj.table_name].foreign_key = parsed_obj.foreign_key      # TODO should be doing more set up here (modifying table referenced from to show that it is referenced by this new table --> for relational integrity)
+                            TABLES[parsed_obj.table_name].foreign_key = parsed_obj.foreign_key
+
+                            if parsed_obj.now:      # if specified, tack on the "now" field to the end of the tuple
+                                TABLES[parsed_obj.table_name].now = True
+                                a = Attribute()
+                                a.name = "now"
+                                a.type = "str"
+                                TABLES[parsed_obj.table_name].attributes.append(a)
+                                TABLES[parsed_obj.table_name].attribute_names.add("now")
+                                TABLES[parsed_obj.table_name].num_attributes += 1
+                                TABLES[parsed_obj.table_name].storage.attr_loc["now"] = TABLES[parsed_obj.table_name].num_attributes - 1
+
 
                             # if foreign key is being set, need to update table being referenced as well
                             if len(parsed_obj.foreign_key) > 0:
@@ -129,12 +132,10 @@ def main():
                 end = time.time()
                 print("Query durations (sec): ", end-start)
         except ValueError:
-            pass
+            pass        # allow recovery from errors
     # end infinite input loop
 
 # END main
-
-
 
 
 
